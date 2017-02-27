2cd284a40e7e0d3ec0b076322d349e220fbcce2f
Kyle Foo <fung933@gmail.com>
2017-02-27 12:56:22 +0800

    initialize first component

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+//index.ios.js - place code here for IOS
+
+//Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+//Create a component
+const App = () => (
+    <Text> Some Text</Text>
+);
+
+//Render it to the device
+AppRegistry.registerComponent('fastapp', () => App);
